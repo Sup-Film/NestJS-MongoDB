40b54ae99eb1057a4cf4c9edd1fe91c4c35223f4
Sup-Film <sup.dongkew@gmail.com>
2025-06-19 16:59:49 +0700

    implement validation in CreateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, IsNumber, IsOptional } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString() // ตรวจสอบว่าเป็น string หรือไม่
   readonly name: string;
+
+  @IsString()
+  @IsOptional() // บอกว่าเป็น optional
   readonly description?: string; // ใช้ ? เพื่อระบุว่าเป็น optional
+
+  @IsNumber()
   readonly price: number;
 }
